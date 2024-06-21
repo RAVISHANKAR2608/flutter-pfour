0d0a2bb88e953017caa185ed8cd9d1ed2e96d885
RAVISHANKAR2608 <ravisaravanan2608@gmail.com>
2024-06-21 11:23:12 +0530

    Time Line Event

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'timeline_event.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,119 +8,90 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Horizontal Timeline Example',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const HorizontalTimeline(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class HorizontalTimeline extends StatefulWidget {
+  const HorizontalTimeline({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HorizontalTimeline> createState() => _HorizontalTimelineState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HorizontalTimelineState extends State<HorizontalTimeline> {
+  int? selectedIndex;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  List<String> generateTimeSlots() {
+    List<String> timeSlots = [];
+    int startHour = 10; // 10:00 AM
+    int endHour = 21; // 9:00 PM
+    for (int hour = startHour; hour <= endHour; hour++) {
+      String period = hour < 12 ? 'AM' : 'PM';
+      int displayHour = hour % 12;
+      if (displayHour == 0) displayHour = 12;
+      timeSlots.add('${displayHour}:00 $period');
+    }
+    return timeSlots;
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    List<String> timeSlots = generateTimeSlots();
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Horizontal Timeline'),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+        child: Stack(
+          children: [
+            Positioned.fill(
+              child: Align(
+                alignment: Alignment.center,
+                child: Container(
+                  height: 2,
+                  color: Colors.grey,
+                ),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: timeSlots.length,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child: TimeSlot(
+                    time: timeSlots[index],
+                    selected: selectedIndex == index,
+                    onTap: () {
+                      setState(() {
+                        selectedIndex = index;
+                      });
+                    },
+                  ),
+                );
+              },
             ),
+            if (selectedIndex != null)
+              Positioned(
+                bottom: 16.0,
+                left: MediaQuery.of(context).size.width / 2 - 75,
+                child: Text(
+                  'Selected Time: ${timeSlots[selectedIndex!]}',
+                  style: const TextStyle(fontSize: 18),
+                ),
+              ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
---
+++ lib/timeline_event.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class TimeSlot extends StatelessWidget {
+  final String time;
+  final bool selected;
+  final VoidCallback onTap;
+
+  TimeSlot({
+    required this.time,
+    required this.selected,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            time,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: selected ? Colors.blue : Colors.black,
+            ),
+          ),
+          const SizedBox(height: 8.0),
+          Container(
+            height: 20,
+            width: 2,
+            color: selected ? Colors.blue : Colors.grey,
+          ),
+        ],
+      ),
+    );
+  }
+}
